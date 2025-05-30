6d7c6910fcf1e831fa8d9596a89511a8b15e1165
Ritu Dhanda <ritudhanda1507@gmail.com>
2025-05-30 12:18:19 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "simple_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0"   # Amazon Linux 2 AMI in us-east-1 (update for your region)
+  ami           = "ami-0953476d60561c955"   # Amazon Linux 2 AMI in us-east-1 (update for your region)
   instance_type = "t2.micro"
-  key_name      = "my-key-pair"             # Your SSH key pair registered in AWS
+  key_name      = "rdd"             # Your SSH key pair registered in AWS
 
   tags = {
     Name = "SimpleEC2"
